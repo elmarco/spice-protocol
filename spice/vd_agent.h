e27cb334cf70409c7e32be6f061921b4b317be17
Alon Levy <alevy@redhat.com>
2010-08-30 13:25:30 +0300

    move pipe header defines from reds.c to spice-protocol

--- spice/vd_agent.h
+++ spice/vd_agent.h
@@ -35,6 +35,16 @@
 
 #include <spice/start-packed.h>
 
+enum {
+    VDP_CLIENT_PORT = 1,
+    VDP_SERVER_PORT,
+};
+
+typedef struct SPICE_ATTR_PACKED VDIChunkHeader {
+    uint32_t port;
+    uint32_t size;
+} VDIChunkHeader;
+
 typedef struct SPICE_ATTR_PACKED VDAgentMessage {
     uint32_t protocol;
     uint32_t type;
